3d0525c6532c795d398f2a707e916445bab819cd
Huong <huongnguyen.nth1709@gmail.com>
2023-02-14 18:04:56 +0200

    implement function newCard and modify function renderGame

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let firstCard = 6;
 let secondCard = 9;
+let cards = [firstCard, secondCard];
+
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,9 +11,14 @@ let messageEl = document.getElementById('message-el');
 let sumEl = document.querySelector('#sum-el');
 let cardsEl = document.getElementById('cards-el');
 
-function startGame() {
-  cardsEl.textContent = 'Cards: ' + firstCard + ' ' + secondCard;
+function renderGame() {
+  cardsEl.textContent = 'Cards: ';
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + ' ';
+  }
+
   sumEl.textContent = 'Sum: ' + sum;
+
   if (sum < 21) {
     message = 'Do you want to draw a new card?';
   } else if (sum === 21) {
@@ -21,5 +28,12 @@ function startGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-  messageEl = message;
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 6;
+  cards.push(card);
+  sum += card;
+  renderGame();
 }
