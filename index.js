e2e080d5ffe55614913292788258e98297b608ef
Huong <huongnguyen.nth1709@gmail.com>
2023-02-20 12:59:18 +0200

    implement dealer random card's function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// player
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -10,6 +11,7 @@ let cardsEl = document.getElementById('cards-el');
 
 let startGameBtn = document.getElementById('startGame-btn');
 let newCardBtn = document.getElementById('newCard-btn');
+let showDownBtn = document.getElementById('showDown-btn');
 let resetGameBtn = document.getElementById('resetGame-btn');
 
 let player = {
@@ -19,6 +21,13 @@ let player = {
 
 let playerEl = document.getElementById('player-el');
 playerEl.textContent = player.name + `: €${player.chips}`;
+
+// dealer
+let dealerCards = [];
+let dealerSum = 0;
+
+let dealerSumEl = document.querySelector('#dealerSum-el');
+let dealerCardsEl = document.getElementById('dealerCards-el');
 
 // Make this function returns a random number between 1 and 13
 function getRandomCard() {
@@ -30,17 +39,48 @@ function getRandomCard() {
   else return randomCard;
 }
 
+function renderDealer() {
+  // generate 2 random number cards for dealer
+  let firstDealerCard = getRandomCard();
+  let secondDealerCard = getRandomCard();
+
+  dealerCards = [firstDealerCard, secondDealerCard];
+  dealerSum = firstDealerCard + secondDealerCard;
+
+  // generate more cards untill dealerSum >= 18
+  if (dealerSum < 18) {
+    do {
+      let dealerCard = getRandomCard();
+      dealerCards.push(dealerCard);
+      dealerSum += dealerCard;
+
+      console.log(dealerCards);
+      console.log(dealerSum);
+    } while (dealerSum < 18);
+  }
+
+  console.log(dealerCards);
+  console.log(dealerSum);
+}
+
 function startGame() {
   isAlive = true;
-  // generate 2 random number
+
+  // generate 2 random number cards for player
   let firstCard = getRandomCard();
   let secondCard = getRandomCard();
+
   cards = [firstCard, secondCard];
   sum = firstCard + secondCard;
+
+  renderDealer();
   renderGame();
+
   startGameBtn.disabled = true;
   resetGameBtn.disabled = false;
   newCardBtn.disabled = false;
+  newCardBtn.disabled = false;
+  if (sum > 15) showDownBtn.disabled = false;
 }
 
 function renderGame() {
@@ -62,6 +102,7 @@ function renderGame() {
     isAlive = false;
     startGameBtn.disabled = false;
     newCardBtn.disabled = true;
+    showDownBtn.disabled = true;
   }
   messageEl.textContent = message;
 }
@@ -72,7 +113,29 @@ function newCard() {
     cards.push(card);
     sum += card;
     renderGame();
+    if (sum > 15) showDownBtn.disabled = false;
   }
+}
+
+function showDown() {
+  if (dealerSum > 21 && sum < 22) {
+    message = 'Congratulation! You win!';
+  } else if (sum < 22 && sum > dealerSum) {
+    message = 'Congratulation! You win!';
+  } else message = 'Oops! You lose! Good luck next time!';
+
+  messageEl.textContent = message;
+
+  dealerCardsEl.textContent = 'Dealer Cards: ';
+  for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardsEl.textContent += dealerCards[i] + ' ';
+  }
+
+  dealerSumEl.textContent = 'Dealer Sum: ' + dealerSum;
+
+  newCardBtn.disabled = true;
+  showDownBtn.disabled = true;
+  isAlive = false;
 }
 
 function resetGame() {
@@ -83,4 +146,7 @@ function resetGame() {
   sum = 0;
   cardsEl.textContent = 'Cards: ';
   sumEl.textContent = 'Sum: ';
+
+  dealerCardsEl.textContent = 'Dealer Cards: ';
+  dealerSumEl.textContent = 'Dealer Sum: ';
 }
